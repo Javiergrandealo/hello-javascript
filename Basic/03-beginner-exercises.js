535e15a5494a03dc67aa9b4d4a0b029c54291a08
Javier Grande Alonso <franciscojaviergrandealonso@gmail.com>
2024-07-18 16:31:50 +0200

    ejercicios hechos

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,21 +5,68 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Escribe un comentario en una línea
+// esto es un comentario de una linea
 
 // 2. Escribe un comentario en varias líneas
+/*Esto
+es
+un 
+cometario
+de
+varias
+lineas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
-
+let string = "Esto es un string"
+let boolean = true
+let numero = 12
+let bigInt = 128349847589347234857934287539248570349570342975430985703428956384706n
+let nulls = null
+let indefinido = undefined
+let simbolo =Symbol("Esto es un simbolo")
 // 4. Imprime por consola el valor de todas las variables
+console.log(string)
+console.log(boolean)
+console.log(numero)
+console.log(bigInt)
+console.log(nulls)
+console.log(indefinido)
+console.log(simbolo)
 
 // 5. Imprime por consola el tipo de todas las variables
-
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof numero)
+console.log(typeof bigInt)
+console.log(typeof nulls)
+console.log(typeof indefinido)
+console.log(typeof simbolo)
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+string = "Esto es otro string"
+boolean = false
+numero = 10
+bigInt = 34765874365872346582346578293461235394752305798756893465823475342857n
+nulls = null
+indefinido = undefined
+simbolo = "Esto es otro simbolo"
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
+//va a dar error es q ni lo hago, habria q comentar todo
+
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
+const string2 = "Hola mundo"
+const booleano = false
+const numero2 = 33
+const bigInt2 = 236743657234524365243657348275634827652947562934865234756324897562348956348927n
+const nulls2 = null
+const undefinido2 = undefined
+const simbolo2 = Symbol("Simbolo 2 baby")
+
 // 9. A continuación, modifica los valores de las constantes
+
+//va a dar error es q ni lo hago, habria q comentar todo
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
